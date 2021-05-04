0439da9e82da59bb1057eebf33ab1e22c275f273
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-05-04 13:23:38 -0700

    complete

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,38 +1,108 @@
 <script lang="ts">
-  import { haptic} from "haptic-util";
-import ArrayEx from "./ArrayEx.svelte";
-import Input from "./Input.svelte";
-import SettingsEx from "./SettingsEx.svelte";
-import StoreTable from "./StoreTable.svelte";
-  import WidgetSwitch from "./WidgetSwitch.svelte";
+import { haptic, HapticStore } from "haptic-util";
 
-  let width = 200;
-  let height = 200;
-  $: haptic.setSize(width, height);
+  import { draw, fade } from "svelte/transition";
+  
+  const states = ["triangle", "circle", "octagon"] as const;
+
+  const state = new HapticStore<typeof states[number]>("state", {default: "circle", sync: true});
+
+  const handleClick = () => {
+    $state = states[(states.indexOf($state) + 1) % states.length];
+  };
+
+  haptic.setSize(200,200);
 </script>
 
-<div bind:clientHeight={height} bind:clientWidth={width}>
-  <h1>Haptic Template</h1>
+<div class="outer">
+  <div class="button {$state}">
+    <div class="inner">
+      <svg width="100%" height="100%" viewBox="0 0 106 106">
+        {#if $state === "triangle"}
+          <polygon
+            on:click={handleClick}
+            points="3,96 103,96 53,10"
+            in:draw={{ duration: 200, delay: 200 }}
+            out:draw={{ duration: 200 }}
+            style="stroke: yellow"
+          />
 
-  <h2>Boolean</h2>
-  <StoreTable id="boolean" component={WidgetSwitch} defaultValue={false}/>
-
-  <h2>String</h2>
-  <StoreTable id="string" component={Input} defaultValue={''} />
-  
-  <h2>Object</h2>
-  <StoreTable id="object" component={SettingsEx} defaultValue={{active: false,alive:false}} />
-
-  <h2>Array</h2>
-  <StoreTable id="array" component={ArrayEx} defaultValue={[]} />
-
+          <polygon
+            on:click={handleClick}
+            points="3,96 103,96 53,10"
+            in:fade={{ duration: 200, delay: 400 }}
+            out:fade={{ duration: 200, delay: 200 }}
+            style="fill: yellow"
+          />
+        {:else if $state === "octagon"}
+          <polygon
+            on:click={handleClick}
+            points="99,34 72,7 34,7 7,34 7,72 34,99 72,99 99,72"
+            in:draw={{ duration: 200, delay: 200 }}
+            out:draw={{ duration: 200 }}
+            style="stroke: red"
+          />
+          <polygon
+            on:click={handleClick}
+            points="99,34 72,7 34,7 7,34 7,72 34,99 72,99 99,72"
+            in:fade={{ duration: 200, delay: 400 }}
+            out:fade={{ duration: 200, delay: 200 }}
+            style="fill: red"
+          />
+        {:else if $state === "circle"}
+          <circle
+            on:click={handleClick}
+            cx="53"
+            cy="53"
+            r="50"
+            in:draw={{ duration: 200, delay: 200 }}
+            out:draw={{ duration: 200 }}
+            style="stroke: green"
+          />
+          <circle
+            on:click={handleClick}
+            cx="53"
+            cy="53"
+            r="50"
+            in:fade={{ duration: 200, delay: 400 }}
+            out:fade={{ duration: 200, delay: 200 }}
+            style="fill: green"
+          />
+        {/if}
+      </svg>
+    </div>
+  </div>
 </div>
 
 <style>
-    div {
-        background: white;
-        padding: 1em;
-        width: min-content;
-        height: max-content;
-    }
+  * {
+    border: 0px !important;
+    outline: 0px !important;
+  }
+  .inner {
+    width: 100%;
+    height: 100%;
+  }
+  div.button {
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    padding: 1em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: transparent;
+    outline: 0;
+    height: 100%;
+  }
+
+  .outer {
+    height: 100%;
+    width: 100%;
+  }
+
+  div.button svg * {
+    fill: transparent;
+    stroke-width: 2px;
+  }
 </style>
